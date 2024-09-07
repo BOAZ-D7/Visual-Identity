434723e26bb7ae318bb6b4f282e015fa6737cf72
BOAZ-D7 <sidney.zen@live.com>
2024-09-07 09:28:26 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados =  [
+    {
+    titulo: "Por que ter uma identidade visual?",
+    descricao: "Uma identidade visual forte cria uma conexão emocional com seus clientes, tornando sua marca memorável e diferenciada.", 
+    link: "https://pt.wikipedia.org/wiki/Identidade_visual",
+    tags: "marca logomarca identidade logo logotipo",
+    },
+
+    {
+    titulo: "A importância da digitalização",
+    descricao: "No mundo digital, a presença online é essencial para o sucesso de qualquer negócio. A digitalização permite que sua marca alcance um público maior e gere mais oportunidades de negócio.",
+    link: "//www.bring.ag/blog/importancia-do-google-meu-negocio/#:~:text=A%20import%C3%A2ncia%20do%20Google%20Meu%20Neg%C3%B3cio%20%C3%A9%20muito%20significativa%20dentro,e%20confian%C3%A7a%20para%20os%20usu%C3%A1rios.",
+    tags: "website site digital moderno modernidade",
+    },
+
+    {
+        titulo: "Nossos serviços",
+        descricao: "Design de logotipos, Design de identidade visual, Design de sites e aplicativos, Marketing digital</li>",
+        link : "http://www.instagram.com/boazdesign7/",
+        tags: "preço serviços tabalho quero",
+    },
+]
